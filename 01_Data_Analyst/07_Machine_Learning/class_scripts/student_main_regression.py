e534a309c930ab68e0b26b3154534c23d9d791eb
Trenton J. McKinney <trenton3983@users.noreply.github.com>
2018-05-21 11:14:48 -0700

    Added prediction and stats

--- 01_Data_Analyst/07_Machine_Learning/class_scripts/student_main_regression.py
+++ 01_Data_Analyst/07_Machine_Learning/class_scripts/student_main_regression.py
@@ -13,6 +13,15 @@ ages_train, ages_test, net_worths_train, net_worths_test = age_net_worth_data()
 
 reg = student_reg(ages_train, net_worths_train)
 
+print(f'Net Worth prediction for 47 year old: {reg.predict(47)}')
+print(f'Slope: {reg.coef_}')
+print(f'Intercept: {reg.intercept_}')
+
+print('\n########## Stats on Test Dataset ##########')
+print(f'r-squared score: {reg.score(ages_test, net_worths_test)}')
+print('\n########## Stats on Training Dataset ##########')
+print(f'r-squared score: {reg.score(ages_train, net_worths_train)}\n')
+
 plt.clf()
 plt.scatter(ages_train, net_worths_train, color="b", label="train data")
 plt.scatter(ages_test, net_worths_test, color="r", label="test data")
